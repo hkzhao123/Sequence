ce2f94b398dd5d1a45033d9516d759f11cb1d386
hkzhao123 <1223602915@qq.com>
2018-06-26 09:27:48 +0800

    hai cha yi ge hanshu

--- Sequence.cpp
+++ Sequence.cpp
@@ -26,15 +26,35 @@ len = ss.length();
 cout<<len<<endl;
 }
 int Sequence::numberOf(char base)
-{for(int i=0;i<len;i++)
-{if(str[i]==base)
+{
+for(int l=0;l<len;l++)
+{if(ss[l]==base)
 num++;
 }
-cout<<base<<"of the number is"<<num<<endl;
-num=0;
+cout<<base<<" of the number is "<<num<<endl;
 }
 string Sequence::longestConsecutive()
-{
+{int f[len],max=0;
+f[0]=1;
+string atcg;
+string sss="ATCG";
+for(int j=0;j<4;j++)
+{for(int i=1;i<len;i++)
+{if(ss[i]==ss[i-1]&&ss[i]==sss[j])
+f[i]=f[i-1]+1;
+else f[i]=1;
+if(f[i]>f[max])
+max=i;
+}
+for(int k=0;k<f[max];k++)
+{atcg=sss[j]+atcg;
+}
+cout<<atcg<<endl;
+max=0;
+f[len]=0;
+f[0]=1;
+atcg="";
+}
 }
 string Sequence::longestRepeated()
 {
